690b25bfacb27eb93b4962d8677bbc38eabed64f
ayushtiwary <er.tiwariayush@gmail.com>
2018-06-05 17:08:51 +0530

    added codeship.yml

---
+++ src/main/scala/controller/SamplePOC.scala
@@ -0,0 +1,16 @@
+package controller
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
+import com.amazonaws.services.dynamodbv2.datamodeling.S3Link
+import com.amazonaws.services.dynamodbv2.model._
+
+
+class SamplePOC {
+
+  val dynamoDbEmbeddedRule = new DynamoDbEmbedded
+
+}
